fe4fb6f374a72ecb017071581e168d2b5fa0e166
Joona Kauppila <jokauppi@helsinki.fi>
2021-07-14 08:26:31 +0300

    Osa 3 tehtävät 3.7-3.8

--- puhelinluettelo-backend/index.js
+++ puhelinluettelo-backend/index.js
@@ -1,6 +1,16 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 app.use(express.json())
+
+morgan.token('person', (req, res) => {
+	if (req.method === 'POST') {
+		return JSON.stringify(req.body)
+	}
+	return ' '
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
 
 let persons = [
 	{
@@ -20,18 +30,12 @@ let persons = [
 	}
 ]
 
-const logReq = (req, res) => {
-	console.log(`${req.method} ${req.url} ${res.statusCode} ${res.statusMessage}`)
-}
-
 app.get('/api/info', (req, res) => {
 	res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${Date()}</p>`)
-	logReq(req, res)
 })
 
 app.get('/api/persons', (req, res) => {
 	res.json(persons)
-	logReq(req, res)
 })
 
 app.get('/api/persons/:id', (req, res) => {
@@ -42,34 +46,30 @@ app.get('/api/persons/:id', (req, res) => {
 	} else {
 		res.status(404).end()
 	}
-	logReq(req, res)
 })
 
 app.delete('/api/persons/:id', (req, res) => {
 	const id = Number(req.params.id)
 	persons = persons.filter(person => person.id !== id)
 	res.status(204).end()
-	logReq(req, res)
 })
 
 app.post('/api/persons/', (req, res) => {
-	
-	const newPerson = req.body
-	
-	if(!newPerson.name) {
-		res.status(400).json({error: 'name is missing'})
-	} else if(!newPerson.number) {
-		res.status(400).json({error: 'number is missing'})
-	} else if(persons.find(person => person.name.toLowerCase() === newPerson.name.toLowerCase())) {
-		res.status(409).json({error: 'name must be unique'})
+
+	const newPerson = {...req.body}
+
+	if (!newPerson.name) {
+		res.status(400).json({ error: 'name is missing' })
+	} else if (!newPerson.number) {
+		res.status(400).json({ error: 'number is missing' })
+	} else if (persons.find(person => person.name.toLowerCase() === newPerson.name.toLowerCase())) {
+		res.status(409).json({ error: 'name must be unique' })
 	} else {
-		newPerson.id = Math.floor(Math.random()*1000000000000000) + 1
+		newPerson.id = Math.floor(Math.random() * 1000000000000000) + 1
 		persons = persons.concat(newPerson)
-		
+
 		res.json(newPerson)
 	}
-
-	logReq(req, res)
 })
 
 const PORT = 3001
